02a8ea511a3a384bf4132dd131afbc21117266db
Mike Lubinets <lubinetsm@yandex.ru>
2017-10-13 23:51:31 +0300

    fixed tests

--- InTempore/app/src/androidTest/java/org/devheap/intempore/route/RouteBuilderTest.java
+++ InTempore/app/src/androidTest/java/org/devheap/intempore/route/RouteBuilderTest.java
@@ -7,7 +7,8 @@ import static org.junit.Assert.*;
 public class RouteBuilderTest {
     @Test
     public void addPlace() throws Exception {
-        RouteBuilder routeBuilder = new RouteBuilder("AIzaSyBwProiHgcHKPpBwxZzGpjNfPFzC3Rl3SM");
+        RouteBuilder.mapsApiKey = "AIzaSyBwProiHgcHKPpBwxZzGpjNfPFzC3Rl3SM";
+        RouteBuilder routeBuilder = RouteBuilder.getInstance();
         routeBuilder.addPlace("ChIJYVe_3mn-W0ERom-Xu0sQdls");
         routeBuilder.await();
         RoutePoint point = routeBuilder.getRoutePoint("ChIJYVe_3mn-W0ERom-Xu0sQdls");
